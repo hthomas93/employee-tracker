ad978ddc21f1377f40604532d1ea341ed8be7a72
Harrison Thomas <hthomas146@utexas.edu>
2020-01-28 21:40:39 -0600

    Added basic structure of prompts

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
@@ -13,5 +14,65 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadID);
+    start();
 });
 
+function start() {
+    inquirer
+        .prompt({
+            name: "menuChoice",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Add", "View", "Update", "Exit"]
+        })
+        .then(function (response) {
+            if (response.menuChoice === "Add") {
+                add();
+            }
+            else if (response.menuChoice === "View") {
+                view();
+            }
+            else if (response.menuChoice === "Update") {
+                update();
+            }
+            else if (response.menuChoice === "Exit") {
+                console.log("See you later!");
+                connection.end();
+            }
+        })
+}
+
+function add() {
+    inquirer
+        .prompt({
+            name: "toAdd",
+            type: "list",
+            message: "What would you like to add?",
+            choices: ["Departments", "Roles", "Employees"]
+        })
+        .then(function (response) {
+            console.log(response.toAdd)
+            start();
+        })
+    // Add departments, roles, employees
+}
+
+function view() {
+    inquirer
+        .prompt({
+            name: "toView",
+            type: "list",
+            message: "What would you like to view?",
+            choices: ["Departments", "Roles", "Employees"]
+        })
+        .then(function (response) {
+            console.log(response.toView)
+            start();
+        })
+    // View departments, roles, employees
+}
+
+function update() {
+    console.log("Time to update!");
+    start();
+}
